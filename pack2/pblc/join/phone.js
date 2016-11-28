52fd589ae574c7ce92037a60dea5db39fe42d014
arsenicum32 <zombiehot@yandex.ru>
2016-11-28 16:18:55 +0300

    next

--- pack2/pblc/join/phone.js
+++ pack2/pblc/join/phone.js
@@ -1,28 +1,20 @@
 $('#phone').on('keyup', function(e){
+  var phone = $(this).val();
+  var el = this;
   if(e.keyCode==13 && $(this).css('border-color') == 'rgb(144, 238, 144)'){
-    $(this).css('border' , 'none')
-    $(this).val('');
-    $(this).off('keyup');
-    $(this).off('input');
-    addText({text: "вам пришло смс с именем художника - введите его.. "});
-    superplaceholder({
-        el: document.getElementById('phone'),
-        sentences: [ 'вам пришло смс с именем', 'введите его сюда :)'],
-        options: {
-            letterDelay: 100, // milliseconds
-            sentenceDelay: 1000,
-            startOnFocus: false,
-            loop: false,
-            shuffle: false,
-            showCursor: true,
-            cursor: '|'
-        }
-    });
-    $(this).on('keyup', function(e){
-      if(e.keyCode == 13){
-        alert('fuck you')
+    $.get(`http://localhost:9000/auth/join?username=${user.name}&password=${user.password}&phone=${phone}&ava=${user.ava}` , function(dt){
+      console.log(dt);
+      if(dt.sucsess){
+        //// тут какая-то капча чтоб не дрочить смс сервис...
+        $.get(d.sucsess , function(dd2){
+          dd2.sucsess?
+          step3(dd2.sucsess , el):
+          alert(dd2);
+        });
+      }else{
+        alert(dt)
       }
-    });
+    })
   }
 })
 
@@ -56,3 +48,33 @@ superplaceholder({
         cursor: '|'
     }
 });
+
+function step3( link , el){
+  $(el).css('border' , 'none')
+  $(el).val('');
+  $(el).off('keyup');
+  $(el).off('input');
+  addText({text: "вам пришло смс с именем художника - введите его.. "});
+  superplaceholder({
+      el: document.getElementById('phone'),
+      sentences: [ 'вам пришло смс с именем', 'введите его сюда :)'],
+      options: {
+          letterDelay: 100, // milliseconds
+          sentenceDelay: 1000,
+          startOnFocus: false,
+          loop: false,
+          shuffle: false,
+          showCursor: true,
+          cursor: '|'
+      }
+  });
+  $(el).on('keyup', function(e){
+    var field = $(this).val();
+    if(e.keyCode == 13){
+      $.get( link  + '?code=' + field , function(o){
+        console.log(o);
+        alert(o);
+      });
+    }
+  });
+}
