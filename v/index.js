1e7afadcced7b98d1eaac325cb6be41170373c55
arsenicum32 <zombiehot@yandex.ru>
2016-12-18 04:06:04 +0300

    go

--- v/index.js
+++ v/index.js
@@ -1,1 +1,9 @@
-document.getElementById('timer').innerHTML = 'hawld'
+var time = 5000;
+setInterval(function(){
+  time--;
+  var h = time;
+  var m = h%60;
+  var s = m%60;
+
+  document.getElementById('timer').innerHTML = h+':'+m+':'+s;
+}, 1000)
